e2addc714f13960cd1f784a22f5201c650fc1859
Ivan chiari <Ivan31@mhs.unsyiah.ac.id>
2023-10-03 12:13:49 +0700

    tes

--- wget.py
+++ wget.py
@@ -1,25 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_all_article_urls(url):
+def get_article_urls(url):
     urls = []
-    page = 1
     
+    page = 1
     while True:
         # Mengirim permintaan GET ke halaman indeks dengan nomor halaman tertentu
         response = requests.get(f"{url}?page={page}")
         soup = BeautifulSoup(response.content, 'html.parser')
+        
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        # Mencari semua tautan ('a') dalam halaman
-        for a_tag in soup.find_all('a', href=True):
-            href = a_tag['href']
-            if href.startswith('https://www.kompasiana.com/'):
-                urls.append(href)
-
-        # Jika tidak ada tautan lagi di halaman, keluar dari loop
-        if not soup.find('a', class_='next'):
-            break
-
+        for urls in soup.find_all('a'):
+            if urls.get('href') and urls.get('href').startswith('https://www.kompasiana.com/'):
+                urls.append(urls.get('href'))
+        
         page += 1
     
     return urls
@@ -28,7 +25,7 @@ def get_all_article_urls(url):
 index_url = 'https://www.kompasiana.com/indeks'
 
 # Panggil fungsi untuk mengambil URL
-article_urls = get_all_article_urls(index_url)
+article_urls = get_article_urls(index_url)
 
 # Cetak semua URL yang telah diambil
 for url in article_urls:
